79c18fb77082c1e2fe459382a9edc63144365088
Ann Emily Grégoire <annemily.gregoire@sheertex.com>
2025-12-24 13:22:51 -0500

    Add base URL in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/*',
-          dest: 'assets'
-        }
-      ]
-    })
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: 'src/assets/*',
+    //       dest: 'assets'
+    //     }
+    //   ]
+    // })
   ],
   build: {
-    outDir: 'docs' // Output to a folder named 'my-custom-folder'
-  }
+    outDir: 'docs' // Output build to a folder named 'docs'
+  },
+  base: 'word_solver', // add this before refs to assets in final URL
 })
