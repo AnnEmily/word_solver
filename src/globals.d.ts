16e04469283f1de5dc791739b89d4862164751d2
Ann Emily Grégoire <annemily.gregoire@sheertex.com>
2025-12-25 16:00:40 -0500

    Allow relative imports without file extension + svg

---
+++ src/globals.d.ts
@@ -0,0 +1,12 @@
+// Needed for non-typescript files
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.svg" {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
